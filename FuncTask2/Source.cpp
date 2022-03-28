b6173a4abe2d03026e3eb41108bf617d6bef2e23
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-03-28 20:51:54 +0500

    Add project files.

---
+++ FuncTask2/Source.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+template <typename T> void showArray(T arr[], int length);
+
+void typeOfN(int N) {
+	cout << "INT\n";
+}
+void typeOfN(double N) {
+	cout << "DOUBLE\n";
+}
+
+int sumAB(int A, int B);
+
+void sortRange(int arr[], int length);
+
+void moveArray(int arr[], int length, int n);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	int n, m;
+
+	// Задача 1
+	/*cout << "Задача 1.\n";
+	cout << "10 - ";
+	typeOfN(10);
+	cout << "5.5 - ";
+	typeOfN(5.5);*/
+
+	// Задача 2
+	/*cout << "Задача 2.\nВведите 2 числа: ";
+	cin >> n >> m;
+	cout << "Сумма чисел от " << n << " до " 
+		<< m << " = " << sumAB(n, m) << "\n\n";*/
+
+		// Задача 3
+	/*cout << "Задача 3.\nИзначальный массив:\n";
+	int z3[10];
+	for (int i = 0; i < 10; i++)
+		z3[i] = rand() % (20 + 1 - (-20)) + (-20) + 100;
+	showArray(z3, 10);
+	cout << "Итоговый массив:\n";
+	sortRange(z3, 10);
+	showArray(z3, 10);*/
+
+	// Задача 4
+	cout << "Задача 4.\nИзначальный массив:\n";
+	int z4[10] = {1,2,3,4,5,6,7,8,9,10};
+	showArray(z4, 10);
+	cout << "Введите кол-во сдвигов:";
+	cin >> n;
+	moveArray(z4, 10, n);
+	cout << "Итоговый массив:\n";
+	showArray(z4, 10);
+
+	return 0;
+}
+
+// Задача 2
+int sumAB(int A, int B){
+	if (A >= B)
+		return A;
+	return B + sumAB(A, B - 1);
+}
+
+// Задача 3
+void sortRange(int arr[], int length) {
+	int firstIndex = -1, lastIndex = -1;
+	//int firstIndex = 0, lastIndex = -1;
+	for (int i = 0; i < length; i++)
+		if (arr[i] < 0) {
+			firstIndex = i;
+			break;
+		}
+	for (int i = length - 1; i >= 0; i--)
+		if (arr[i] < 0) {
+			lastIndex = i;
+			break;
+		}
+	if (firstIndex != -1)
+		//Пузырковый метод
+	   for (int i = lastIndex; i > firstIndex; i--)
+		  for (int j = firstIndex; j < i; j++)
+			  if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+		//Быстрая сортировка
+		//sort(arr + firstIndex, arr + lastIndex);
+}
+
+// Задача 4
+void moveArray(int arr[], int length, int n) {
+	if (n >= length)
+		n -= (n / length) * length;
+	for (int j = 0; j < n; j++) {
+		int tmp = arr[length - 1];
+		for (int i = length - 1; i >= 1; i--)
+			arr[i] = arr[i - 1];
+		arr[0] = tmp;
+	}
+}
+
+
+template <typename T> void showArray(T arr[], int length) {
+	cout << "[";
+	for (int i = 0; i < length; i++)
+		cout << arr[i] << ", ";
+	cout << "\b\b]\n";
+}
